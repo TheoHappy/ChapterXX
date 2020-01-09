1647ee75ba65c3fc9f34f3304014069e1282163b
TheoHappy <38621790+TheoHappy@users.noreply.github.com>
2020-01-09 12:38:21 +0300

    fix nonGeneric method

--- src/NonGenericMethod.java
+++ src/NonGenericMethod.java
@@ -1,16 +1,19 @@
-public class NongenericMethod {
+public class NonGenericMethod {
     public static void main(String[] args) {
         // create arrays of Integer, Double and Character
         Integer[] integerArray = {1, 2, 3, 4, 5, 6, 7, 8};
         Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
         Character[] characterArray = {'H', 'E', 'L', 'L', 'O'};
-
-        System.out.printf("Array stringArray contains:%n");
+        String[] stringArray = {"one", "two", "three", "four", "five", "six", "seven", "eight"};
+        
+        System.out.printf("Array integerArray contains:%n");
         printArray(integerArray); // pass an Integer array
         System.out.printf("%nArray doubleArray contains:%n");
         printArray(doubleArray); // pass a Double array
         System.out.printf("%nArray characterArray contains:%n");
         printArray(characterArray); // pass a Character array
+        System.out.printf("%nArray stringArray contains:%n");
+        printArray(stringArray); // pass a String array
     }
 
     public static <T> void printArray(T[] inputArray)
@@ -22,28 +25,28 @@ public class NongenericMethod {
     }
 
 
-    public static void printArray(Integer[] inputArray) {
-        String[] numbers = new String[inputArray.length];
+    public static void printArray(String[] inputArray) {
+        String[] numbers = inputArray;
         for (int i = 0; i < inputArray.length; i++) {
-            numbers[i] = convertNumberToString(inputArray[i]);
             if (i < inputArray.length / 2)
                 System.out.format("%s   ",numbers[i]);
             if (i == inputArray.length / 2)
                 System.out.println();
             if (i >= inputArray.length / 2) {
-                if (numbers[i].length() > numbers[i-inputArray.length/2].length()) {
+                int secondRowLength = numbers[i - inputArray.length / 2].length();
+                if (numbers[i].length() > secondRowLength) {
                     System.out.format("%s%s ",
                             numbers[i],
-                            spaceCreator(numbers[i].length() - numbers[i - inputArray.length / 2].length()));
+                            spaceCreator(numbers[i].length() - secondRowLength));
                 }
 
-                if (numbers[i].length() < numbers[i-inputArray.length/2].length()) {
+                if (numbers[i].length() < secondRowLength) {
                     System.out.format("%s%s   ",
                             numbers[i],
-                            spaceCreator(numbers[i - inputArray.length / 2].length() - numbers[i].length()));
+                            spaceCreator(secondRowLength - numbers[i].length()));
                 }
 
-                if (numbers[i].length() == numbers[i-inputArray.length/2].length()){
+                if (numbers[i].length() == secondRowLength){
                     System.out.format("%s   ",
                             numbers[i]);
                 }
@@ -59,39 +62,5 @@ public class NongenericMethod {
             result = result + " ";
         return result;
     }
-
-
-    public static String convertNumberToString(Integer number) {
-        String result = null;
-        switch (number) {
-            case 1:
-                result = "one";
-                break;
-            case 2:
-                result = "two";
-                break;
-            case 3:
-                result = "three";
-                break;
-            case 4:
-                result = "four";
-                break;
-            case 5:
-                result = "five";
-                break;
-            case 6:
-                result = "six";
-                break;
-            case 7:
-                result = "seven";
-                break;
-            case 8:
-                result = "eight";
-                break;
-            case 9:
-                result = "nine";
-                break;
-        }
-        return result;
-    }
+   
 }
